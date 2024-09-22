78f1a9bb93fcd8c528a92c9f764965e3a9ee4b5c
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-09-22 00:54:47 -0400

    allow run.sh to run arbitrary scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # Evaluate .env file into shell env (temporarily, for just this line)
-# Run hello.go
-env $(grep -v "^ *#" -E .env | xargs) go run hello.go
+# Run <first arg>
+# Ex: ./run.sh cmd.go
+env $(grep -v "^ *#" -E .env | xargs) go run $1
