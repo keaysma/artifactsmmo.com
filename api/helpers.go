b3cb0bfea4d750c4b6bcab64e7ec37fa19dc48bb
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-09-26 01:10:07 -0400

    revise cooldown to use datetime

--- api/helpers.go
+++ api/helpers.go
@@ -10,17 +10,25 @@ import (
 )
 
 func WaitForDown(cooldown types.Cooldown) {
-	if cooldown.Remaining_seconds > 0 {
-		new_data := state.CooldownData{
-			Current: float64(cooldown.Remaining_seconds),
-			Max:     float64(cooldown.Total_seconds),
-		}
-
-		data := state.GlobalCooldown.Ref()
-		*data = new_data
-		state.GlobalCooldown.Unlock()
-
-		utils.Log(fmt.Sprintf("Cooldown remaining: %d", cooldown.Remaining_seconds))
-		time.Sleep(time.Duration(cooldown.Remaining_seconds) * time.Second)
+	if cooldown.Remaining_seconds <= 0 {
+		return
 	}
+
+	end, err := time.Parse(time.RFC3339, cooldown.Expiration)
+	if err != nil {
+		utils.Log(fmt.Sprintf("Failed to parse cooldown expiration: %s", err))
+		return
+	}
+
+	new_cooldown := state.CooldownData{
+		Duration_seconds: cooldown.Total_seconds,
+		End:              &end,
+	}
+
+	state.GlobalCooldown.With(func(value *state.CooldownData) *state.CooldownData {
+		return &new_cooldown
+	})
+
+	utils.Log(fmt.Sprintf("Cooldown remaining: %d", cooldown.Remaining_seconds))
+	time.Sleep(time.Duration(cooldown.Remaining_seconds) * time.Second)
 }
