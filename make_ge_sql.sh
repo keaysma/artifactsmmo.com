83aba27efee0e3ef4a6a8e45e73aacfe70ad9622
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-10-14 22:10:36 -0400

    Data backup

--- make_ge_sql.sh
+++ make_ge_sql.sh
@@ -2,3 +2,6 @@
 
 # CREATE TABLE orderbook (timestamp DATETIME, code TEXT, stock INTEGER, sell_price INTEGER, buy_price INTEGER, PRIMARY KEY (timestamp, code));
 # WITH X AS (SELECT code, MAX(timestamp) as tx FROM orderbook GROUP BY code) SELECT O.* FROM orderbook O JOIN X on O.timestamp = X.tx;
+
+# CREATE TABLE transactions (timestamp DATETIME, code TEXT, quantity INTEGER, price INTEGER, side TEXT, PRIMARY KEY (timestamp, code));
+# CREATE TABLE market_parameters (code TEXT, theo INTEGER, max_stock INTEGER. min_stock INTEGER, PRIMARY KEY (code));
