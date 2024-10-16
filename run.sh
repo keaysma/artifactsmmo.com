01a21bf5405157ca6290f7db502cef1c53963507
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-10-15 23:24:15 -0400

    run.sh support main.go

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # Evaluate .env file into shell env (temporarily, for just this line)
 # Run <first arg>
 # Ex: ./run.sh cmd.go
-env $(grep -v "^ *#" -E .env | xargs) go run $1
+env $(grep -v "^ *#" -E .env | xargs) go run $1 $2
