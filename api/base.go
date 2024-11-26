669dabd0f9cf5431718c6da438c52838ed0557da
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-11-25 20:30:58 -0500

    new POST and GET API handles

--- api/base.go
+++ api/base.go
@@ -7,6 +7,7 @@ import (
 	"io"
 
 	"artifactsmmo.com/m/utils"
+	"github.com/mitchellh/mapstructure"
 )
 
 // structure:
@@ -58,6 +59,61 @@ func GetDataResponse(url string, params Params) (*DataResponse, error) {
 	return &data, nil
 }
 
+func GetDataResponseFuture[T interface{}](url string, params interface{}, response *T) error {
+	var parsedParams *map[string]string
+
+	switch typedParams := params.(type) {
+	case nil:
+		parsedParams = &map[string]string{}
+	case *map[string]string:
+		parsedParams = typedParams
+	default:
+		utils.DebugLog("decoding interface")
+		marshalledParamsRef, err := utils.MarshallParams(&typedParams)
+		if err != nil {
+			return err
+		}
+
+		utils.DebugLog(fmt.Sprintf("marshalled params: %s", marshalledParamsRef))
+		parsedParams = marshalledParamsRef
+	}
+
+	res, err := utils.HttpGet(
+		url,
+		map[string]string{},
+		parsedParams,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	text, err := io.ReadAll(res.Body)
+	utils.DebugLog(fmt.Sprintf("response: %s", text))
+
+	if err != nil {
+		return err
+	}
+
+	var data DataResponse
+	var uerr = json.Unmarshal(text, &data)
+
+	if uerr != nil {
+		return err
+	}
+
+	if data.Data == nil {
+		return fmt.Errorf("%s", data)
+	}
+
+	err = mapstructure.Decode(data.Data, response)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func PostDataResponse(url string, body Body) (*DataResponse, error) {
 	rawBody, err := json.Marshal(body)
 	if err != nil {
@@ -101,4 +157,59 @@ func PostDataResponse(url string, body Body) (*DataResponse, error) {
 	}
 
 	return &data, nil
+}
+
+func PostDataResponseFuture[T interface{}](url string, body Body, response *T) error {
+	var rawBody []byte
+	var err error
+
+	if body != nil {
+		rawBody, err = json.Marshal(body)
+		if err != nil {
+			return err
+		}
+	}
+
+	res, err := utils.HttpPost(
+		url,
+		map[string]string{},
+		bytes.NewReader(rawBody),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	sc := res.StatusCode
+	if sc > 299 {
+		text := ""
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			text = "unabled to decode!"
+		} else {
+			text = string(b)
+		}
+
+		return fmt.Errorf("error: %d, %s", sc, text)
+	}
+
+	text, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return err
+	}
+
+	var data DataResponse
+	uerr := json.Unmarshal(text, &data)
+
+	if uerr != nil {
+		return err
+	}
+
+	err = mapstructure.Decode(data.Data, response)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- utils/json.go
+++ utils/json.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"strings"
 )
 
 func PrettyPrint(data interface{}) string {
@@ -13,4 +15,50 @@ func PrettyPrint(data interface{}) string {
 	}
 
 	return string(out)
+}
+
+func MarshallParams[I interface{}](in *I) (*map[string]string, error) {
+	/*
+		Convert some params struct into lower-case JSON
+
+		Ex:
+		type GetSellOrdersParams struct {
+			Code   string
+			Seller string
+		}
+
+		Becomes:
+		map[string]string{
+			"code": GetSellOrdersParams.Code,
+			"seller": GetSellOrdersParams.Seller,
+		}
+
+		Empty values are omitted
+	*/
+
+	if in == nil {
+		return nil, nil
+	}
+
+	inv := *in
+	v := reflect.ValueOf(inv)
+	o := map[string]string{}
+
+	for i := range v.NumField() {
+		name := v.Type().Field(i).Name
+
+		valueRef := v.Field(i)
+		if valueRef.Type().String() != "string" && valueRef.Type().String() != "int" {
+			return nil, fmt.Errorf("field %s is not a string, it is %s", name, valueRef.Type().String())
+		}
+
+		value := valueRef.String()
+		if value == "" {
+			continue
+		}
+
+		o[strings.ToLower(name)] = value
+	}
+
+	return &o, nil
 }
