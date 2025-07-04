45d3a505721cd10d5dc4676523b777cc836bb18a
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2025-07-04 10:09:10 -0400

    reflect only access public fields

--- utils/reflect.go
+++ utils/reflect.go
@@ -1,9 +1,14 @@
 package utils
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 func GetFieldFromStructByName(obj interface{}, name string) reflect.Value {
+	safe_name := fmt.Sprintf("%s%s", strings.ToUpper(name[0:1]), name[1:])
 	r := reflect.ValueOf(obj)
-	f := reflect.Indirect(r).FieldByName(name)
+	f := reflect.Indirect(r).FieldByName(safe_name)
 	return f
 }
